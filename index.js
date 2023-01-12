057327d4272baebc906aec99ce98ee97e88b81ac
Jonny254 <buchinskia@gmail.com>
2023-01-12 15:51:25 +0200

    create front

--- index.js
+++ index.js
@@ -1,23 +1,21 @@
+
 const axios = require('axios');
 const cheerio = require('cheerio');
 const {formToJSON} = require("axios");
 
 axios.get('https://epicentrk.ua/ua/shop/metallocherepitsa/').then(({data}) => {
     const $ = cheerio.load(data);
-    let obj;
+
     const materials = $('.card')
         .map((_, material) => {
             const $material = $(material);
             const name = $material.find('.card__name').text();
             const price = $material.find('.card__price-row').text();
 
-            return obj = {'name': name,'price': price};
+            return JSON.stringify({'name': name,'price': price});
         })
-
-    // localStorage.setItem("materials", JSON.stringify(materials));
+        .toArray()
+        //localStorage.setItem("materials", JSON.stringify(materials));
     //const myJson = JSON.stringify(obj);
-
-    console.log(obj);
-
-    //console.log(json);
+    console.log(materials);
 });
